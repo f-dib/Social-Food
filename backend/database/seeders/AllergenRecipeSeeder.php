cce357c7ca53769c570b48e88ef4764bab92df31
federico-dibitonto <f.dibitonto94@gmail.com>
2024-07-12 18:38:26 +0200

    fixed allergenrecipeseeder

--- backend/database/seeders/AllergenRecipeSeeder.php
+++ backend/database/seeders/AllergenRecipeSeeder.php
@@ -15,26 +15,38 @@ class AllergenRecipeSeeder extends Seeder
      */
     public function run(): void
     {
-        // Fetch all recipes and allergens
-        $recipes = Recipe::all();
-        $allergens = Allergen::all();
-
-        // Array to hold the allergen_recipe relationships
-        $allergenRecipes = [];
-
-        // Loop through each recipe
-        foreach ($recipes as $recipe) {
-            // Select random allergens for this recipe
-            $randomAllergens = $allergens->random(rand(1, 14));
-
-            foreach ($randomAllergens as $allergen) {
-                // Add the relationship to the array
-                $allergenRecipes[] = [
-                    'recipe_id' => $recipe->id,
-                    'allergen_id' => $allergen->id,
-                ];
-            }
-        }
+        $allergenRecipes = [
+            ['recipe_id' => 1, 'allergen_id' => 3], // Pasta alla Carbonara - Latte e derivati
+            ['recipe_id' => 1, 'allergen_id' => 9], // Pasta alla Carbonara - Glutine
+            ['recipe_id' => 1, 'allergen_id' => 14], // Pasta alla Carbonara - Uova e derivati
+        
+            ['recipe_id' => 2, 'allergen_id' => 3], // Tiramisù - Latte e derivati
+            ['recipe_id' => 2, 'allergen_id' => 14], // Tiramisù - Uova e derivati
+            ['recipe_id' => 2, 'allergen_id' => 9], // Tiramisù - Glutine
+        
+            ['recipe_id' => 3, 'allergen_id' => 9], // Risotto alla Milanese - Glutine
+            ['recipe_id' => 3, 'allergen_id' => 3], // Risotto alla Milanese - Latte e derivati
+        
+            ['recipe_id' => 4, 'allergen_id' => 3], // Panna Cotta - Latte e derivati
+        
+            ['recipe_id' => 5, 'allergen_id' => 9], // Lasagne - Glutine
+            ['recipe_id' => 5, 'allergen_id' => 3], // Lasagne - Latte e derivati
+            ['recipe_id' => 5, 'allergen_id' => 14], // Lasagne - Uova e derivati
+        
+            ['recipe_id' => 6, 'allergen_id' => 3], // Cannoli Siciliani - Latte e derivati
+            ['recipe_id' => 6, 'allergen_id' => 14], // Cannoli Siciliani - Uova e derivati
+            ['recipe_id' => 6, 'allergen_id' => 9], // Cannoli Siciliani - Glutine
+        
+            ['recipe_id' => 7, 'allergen_id' => 9], // Pizza Margherita - Glutine
+            ['recipe_id' => 7, 'allergen_id' => 3], // Pizza Margherita - Latte e derivati
+        
+            ['recipe_id' => 8, 'allergen_id' => 3], // Gelato - Latte e derivati
+        
+            ['recipe_id' => 9, 'allergen_id' => 9], // Spaghetti al Pomodoro - Glutine
+        
+            ['recipe_id' => 10, 'allergen_id' => 9], // Pesto alla Genovese - Glutine
+            ['recipe_id' => 10, 'allergen_id' => 13], // Pesto alla Genovese - Anidride solforosa e solfiti
+        ];
 
         // Insert the generated relationships into the pivot table
         DB::table('allergen_recipe')->insert($allergenRecipes);
