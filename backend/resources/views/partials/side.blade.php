b344d118ff2ff019c8a4c1335e0249e0e6587f5f
Emanuele <manu.pogliari@gmail.com>
2024-07-18 13:45:59 +0200

    component sidebar created

---
+++ backend/resources/views/partials/side.blade.php
@@ -0,0 +1,17 @@
+<div id="my-aside" class="text-center">
+    <ul class="border border-b-orange-300">
+        <li class="p-8 text-black dark:text-white">Home</li>
+        <li class="p-8 text-black dark:text-white">Aggiungi Ricette</li>
+        <li class="p-8 text-black dark:text-white">Messaggi</li>
+    </ul>
+    <ul class="border"> 
+        <li class="p-8 text-black dark:text-white">Statistiche</li>
+        <li class="p-8 text-black dark:text-white">Notifiche</li>
+        <li class="p-8 text-black dark:text-white">Content 6</li>
+    </ul>
+    <ul>
+        <li class="p-8 text-black dark:text-white">Impostazioni</li>
+        <li class="p-8 text-black dark:text-white">Logout</li>
+        <li class="p-8 text-black dark:text-white">Versione 0.1</li>
+    </ul>
+</div>
