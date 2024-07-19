9a28974be38cdb45013b9a80a5b2ed02af709d5a
mattia_morriale <mattia.morriale1@gmail.com>
2024-07-19 12:18:25 +0200

    mod button create recipes

--- backend/resources/views/recipes/index.blade.php
+++ backend/resources/views/recipes/index.blade.php
@@ -17,10 +17,11 @@
         </div>
     </div>
 
-    <div class="flex flex-wrap justify-start gap-4 py-7">
+    <div class="flex flex-wrap justify-between gap-4 py-7">
         
         <h1 class="text-3xl text-center text-gray-900 dark:text-white sm:text-5xl">Ricette</h1>
-    
+
+        <a  class="text-white hover:text-white border hover:border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800" href="{{ route('admin.recipes.create')}}">Nuova Ricetta</a>
     </div>
     
     <div class="flex flex-wrap justify-center gap-4 pt-7">
@@ -34,12 +35,6 @@
             </div>
         @endforeach
 
-        <div class="block w-full p-6 bg-white border border-gray-200 shadow rounded-3xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <a href="{{ route('admin.recipes.create')}}">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nuova Ricetta</h5>
-            </a>
-        </div>
-    
     </div>
 
 </div>
