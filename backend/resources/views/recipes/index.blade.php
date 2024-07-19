20cf3ea004ee905b941d192f22b1754745e8587d
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-07-19 11:43:22 +0200

    add routes in recipes index

--- backend/resources/views/recipes/index.blade.php
+++ backend/resources/views/recipes/index.blade.php
@@ -26,11 +26,15 @@
     <div class="flex flex-wrap justify-center gap-4 pt-7">
     
         @foreach ($recipes as $recipe)
-            <div href="#" class="block w-full p-6 bg-white border border-gray-200 shadow rounded-3xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $recipe->name }}</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">{{ $recipe->description }}</p>
+            <div href="{{ route('admin.recipes.show', $recipes) }}" class="block w-full p-6 bg-white border border-gray-200 shadow rounded-3xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $recipe->name }}</h5>
+                <p class="font-normal text-gray-700 dark:text-gray-400">{{ $recipe->description }}</p>
             </div>
         @endforeach
+
+        <div href="{{ route('admin.recipes.create') }}" class="block w-full p-6 bg-white border border-gray-200 shadow rounded-3xl hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nuova Ricetta</h5>
+        </div>
     
     </div>
 
